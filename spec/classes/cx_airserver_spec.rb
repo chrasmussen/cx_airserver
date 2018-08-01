0d573be686f1d2da2b01d7edef52032b63726799
Chris Rasmussen <chris.rasmussen@connexta.com>
2018-08-01 09:13:05 -0700

    removed contents of spec file

--- spec/classes/cx_airserver_spec.rb
+++ spec/classes/cx_airserver_spec.rb
@@ -1,38 +1,4 @@
 require 'spec_helper'
 
 describe 'cx_airserver' do
-  on_supported_os.each do |os, os_facts|
-    context "on #{os}" do
-      let(:facts) { os_facts }
-
-      it { is_expected.to compile }
-      it { is_expected.to contain_file('/opt/connexta/cx_airserver').with(
-	  'ensure' => 'directory',
-          'owner'  => 'root',
-          'group'  => 'wheel',
-          'mode'   => '0700',
-          'before' => 'File[/opt/connexta/cx_airserver/scripts]',
-            ) }
-      it { is_expected.to contain_file('/opt/connexta/cx_airserver/scripts').with(
-          'ensure' => 'directory',
-          'owner'  => 'root',
-          'group'  => 'wheel',
-          'mode'   => '0700',
-          'before' => 'File[/opt/connexta/cx_airserver/scripts/airserver.sh]',
-            ) }
-      it { is_expected.to contain_file('/Library/LaunchAgents/com.connexta.airserver.plist').with(
-          'owner'  => 'root',
-          'group'  => 'wheel',
-          'mode'   => '0644',
-          'source' => 'puppet:///modules/cx_airserver/com.connexta.airserver.plist',
-            ) }
-      it { is_expected.to contain_file('/opt/connexta/cx_airserver/scripts/airserver.sh').with(
-          'owner'  => 'root',
-          'group'  => 'wheel',
-          'mode'   => '0755',
-          'source' => 'puppet:///modules/cx_airserver/airserver.sh',
-          'before' => 'File[/Library/LaunchAgents/com.connexta.airserver.plist]',
-            ) }
-    end
-  end
 end
