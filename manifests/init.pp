97307d4833528dfb746ffe734bc309b7d78a386e
Chris Rasmussen <chris.rasmussen@connexta.com>
2018-08-01 08:58:19 -0700

    fixed it with Heston's help this time

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,33 +5,33 @@
 # @example
 #   include cx_airserver
 class cx_airserver {
-    file{ '/opt/connexta/cx_airserver':
-    	ensure => directory,
-        owner  => 'root',
-        group  => 'wheel',
-        mode   => '0700',
-	before => File['/opt/connexta/cx_airserver/scripts'],
-    }
-    file{ '/opt/connexta/cx_airserver/scripts':
-    	ensure => directory,
-        owner  => 'root',
-        group  => 'wheel',
-        mode   => '0700',
-        before => File['/opt/connexta/cx_airserver/scripts/airserver.sh'],
-    }
-    file { '/opt/connexta/cx_airserver/scripts/airserver.sh':
-	ensure => 'file',
-        owner  => 'root',
-        group  => 'wheel',
-        mode   => '0755',
-        source => 'puppet:///modules/cx_airserver/airserver.sh',
-        before => File['/Library/LaunchAgents/com.connexta.airserver.plist'],
-    }
-    file { '/Library/LaunchAgents/com.connexta.airserver.plist':
-	ensure => 'file',
-        owner  => 'root',
-        group  => 'wheel',
-        mode   => '0644',
-        source => 'puppet:///modules/cx_airserver/com.connexta.airserver.plist',
-    }
+  file{ '/opt/connexta/cx_airserver':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'wheel',
+    mode   => '0700',
+    before => File['/opt/connexta/cx_airserver/scripts'],
+  }
+  file{ '/opt/connexta/cx_airserver/scripts':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'wheel',
+    mode   => '0700',
+    before => File['/opt/connexta/cx_airserver/scripts/airserver.sh'],
+  }
+  file { '/opt/connexta/cx_airserver/scripts/airserver.sh':
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'wheel',
+    mode   => '0755',
+    source => 'puppet:///modules/cx_airserver/airserver.sh',
+    before => File['/Library/LaunchAgents/com.connexta.airserver.plist'],
+  }
+  file { '/Library/LaunchAgents/com.connexta.airserver.plist':
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'wheel',
+    mode   => '0644',
+    source => 'puppet:///modules/cx_airserver/com.connexta.airserver.plist',
+  }
 }
